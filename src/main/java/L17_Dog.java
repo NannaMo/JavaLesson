266504811dca9c2867e94a7f67a607f00f640bef
NannaMo <anna.mostypan@gmail.com>
2024-11-03 18:48:28 +0200

    Lesson 21 Studying Enum

--- src/main/java/L17_Dog.java
+++ src/main/java/L17_Dog.java
@@ -7,30 +7,33 @@ public class L17_Dog {
         System.out.println("Dog's count is " + dogsCount);
     }
 
-    private int paws = 4; // private - запрещает напрямую модифицировать значение при использовании в других классах.
-    private int tail = 1;
+    public static final int PAWS = 4;
+    // private - запрещает напрямую модифицировать значение
+    // при использовании в других классах.
+    //final - константа !!!
+    public static final int TAIL = 1;
     private String name;
     private String breed;
-    private String size;
-
+    private L21_Size size;
 
 
     public static int getDogsCount() {
         return dogsCount;
     }
 
-    public String getSize() {
+    public L21_Size getSize() {
         return size;
     }
 
-    public void setSize(String size) {
-        if (size.equalsIgnoreCase("Big") ||
+    public void setSize(L21_Size size) {
+        this.size = size;
+       /* if (size.equalsIgnoreCase("Big") ||
                 size.equalsIgnoreCase("Average") ||
                 size.equalsIgnoreCase("Small")) {
             this.size = size;
         } else {
             System.out.println("Size should be one of these: Big, Average or Small");
-        }
+        }*/
     }
 //Shift + 6 - исправляет опечатки во всех связях (предварительно нужноыделить слово).
 
@@ -42,34 +45,18 @@ public class L17_Dog {
         return name;
     }
 
-    public void setPaws(int paws) {
-        if (paws == 4) {
-            this.paws = paws;
-        } else {
-            System.out.println("User tried to assign " + paws + " paws for a dog");
-            System.out.println("Correct number is 4");
-        }
 
-        // Сгенерировать set и get можно комбинацией Alt+Insert
+    // Сгенерировать set и get можно комбинацией Alt+Insert
 
-    }
 
     public int getPaws() {
-        return paws;
+        return PAWS;
     }
 
     public int getTail() {
-        return tail;
+        return TAIL;
     }
 
-    public void setTail(int tail) {
-        if (tail == 1) {
-            this.tail = tail;
-        } else {
-            System.out.println("User tried to assign " + tail + " tails for a dog");
-            System.out.println("Correct number is 1");
-        }
-    }
 
     public String getBreed() {
         return breed;
@@ -80,12 +67,18 @@ public class L17_Dog {
     }
 
     public void bark() {
-        if (size.equalsIgnoreCase("Big")) {
-            System.out.println("Wof - Wof");
-        } else if (size.equalsIgnoreCase("Average")) {
-            System.out.println("Raf - Raf");
-        } else {
-            System.out.println("Tiaf - Tiaf");
+        switch (size) {
+            case BIG:
+            case VERY_BIG:
+                System.out.println("Wof - Wof");
+                break;
+            case AVERAGE:
+                System.out.println("Raf - Raf");
+                break;
+            case SMALL:
+            case VERY_SMALL:
+                System.out.println("Tiaf - Tiaf");
+                break;
         }
     }
 
--- src/main/java/L17_Main.java
+++ src/main/java/L17_Main.java
@@ -4,27 +4,22 @@ public class L17_Main {
         System.out.println("Dog's count " + L17_Dog.getDogsCount());
 
         L17_Dog lab = new L17_Dog();
-        lab.setPaws(4);
-        lab.setTail(1);
         lab.setName("Charley");
         lab.setBreed("Lab");
-        lab.setSize("average");
+        lab.setSize(L21_Size.AVERAGE);
         lab.bite();
 
         // Ctrl + Shift + / - закомментировать область
         L17_Dog sheppard = new L17_Dog();
-        sheppard.setPaws(4);
-        sheppard.setTail(1);
         sheppard.setName("Mike");
         sheppard.setBreed("Sheppard");
-        sheppard.setSize("biG");
+        sheppard.setSize(L21_Size.BIG);
         sheppard.bite();
 
         L17_Dog doberman = new L17_Dog();
-        doberman.setPaws(4);
-        doberman.setTail(1);
         doberman.setName("Jack");
         doberman.setBreed("Doberman");
+        doberman.setSize(L21_Size.BIG);
         doberman.bite();
 
         /*System.out.println("Lab's name is " + lab.getName());
@@ -32,5 +27,7 @@ public class L17_Main {
         System.out.println("Lab has " + lab.getPaws() + " paws");
         System.out.println("Sheppard has " + sheppard.getTail() + " tail");*/
     }
+
+
 
 }
