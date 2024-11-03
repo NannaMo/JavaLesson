782e2e4bfeb0756fbbd8eb35abaf23fbb89ccaeb
NannaMo <anna.mostypan@gmail.com>
2024-11-03 11:34:16 +0200

    Lesson 18 Studying Classes and Objects part 2

--- src/main/java/L17_Dog.java
+++ src/main/java/L17_Dog.java
@@ -6,4 +6,48 @@ public class L17_Dog {
 
 //Shift + 6 - исправляет опечатки во всех связях (предварительно нужноыделить слово).
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPaws(int paws) {
+        if (paws == 4) {
+            this.paws = paws;
+        } else {
+            System.out.println("User tried to assign " + paws + " paws for a dog");
+            System.out.println("Correct number is 4");
+        }
+
+        // Сгенерировать set и get можно комбинацией Alt+Insert
+
+    }
+
+    public int getPaws() {
+        return paws;
+    }
+
+    public int getTail() {
+        return tail;
+    }
+
+    public void setTail(int tail) {
+        if (tail == 1) {
+            this.tail = tail;
+        } else {
+            System.out.println("User tried to assign " + tail + " tails for a dog");
+            System.out.println("Correct number is 1");
+        }
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
 }
