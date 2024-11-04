fbbda6ae7a4673790df86b11a1b8463d2bc71f4c
NannaMo <anna.mostypan@gmail.com>
2024-11-04 11:04:20 +0200

    Lesson 24 Studying Constructors part 2

--- src/main/java/constructors/L23_Constructors.java
+++ src/main/java/constructors/L23_Constructors.java
@@ -5,8 +5,14 @@ import domainmodel.L23_Employee;
 public class L23_Constructors {
     public static void main(String[] args) {
 
-        L23_Employee employee = new L23_Employee("Anna", "Developer", 100);
+        L23_Employee employee = new L23_Employee();
+      /*  L23_Employee employee2 = new L23_Employee("Anna", "Developer", 100);
+        L23_Employee employee1 = new L23_Employee("John", "Developer", 100);
 
         System.out.println(employee);
+        System.out.println(employee1);*/
+
+
+
     }
 }
